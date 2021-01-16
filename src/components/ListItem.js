a95f0677da6a6bbbf7aae60bd3857a90b3fe1dd8
pedrohvbsilva <pedrohvbsilva@gmail.com>
2021-01-16 20:50:52 -0300

    css changes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -9,7 +9,7 @@ const ListItem = ({ character }) => {
             {name}
         </div>
         <ul className='character-series'>
-            {series.items.length === 0 ? 'Sem series' : series.items.slice(0,3).map(serie => <li key={serie.key}>{serie.name}</li>)}
+            {series.items.length === 0 ? 'Sem series' : series.items.slice(0,3).map(serie => <li key={serie.key}>{serie.name.split('(')[0]}</li>)}
         </ul>
         <ul className='character-events'>
             {events.items.length === 0 ? 'Sem eventos' : events.items.slice(0,3).map(event => <li key={event.key}>{event.name}</li>)}
--- src/marvelApi.js
+++ src/marvelApi.js
@@ -4,6 +4,6 @@ const PUBLIC_KEY = '83fb155d7bfe832669970173e4ecdaa6';
 const PRIVATE_KEY = 'd2630d393637a3d3a412a380b03975f8b0ed16f6';
 const ts = Date.now().toString();
 const hash = md5(ts+PRIVATE_KEY+PUBLIC_KEY);
-const MARVEL_API = `${MARVEL_URL}ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&limit=4`;
+const MARVEL_API = `${MARVEL_URL}ts=${ts}&apikey=${PUBLIC_KEY}&hash=${hash}&limit=50`;
 
 export default MARVEL_API
