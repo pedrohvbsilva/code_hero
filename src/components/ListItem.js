0333e1a2243c7b1bf4ecc2a6967eb27c16685a17
pedrohvbsilva <pedrohvbsilva@gmail.com>
2021-01-17 14:43:39 -0300

    Add char-overview effect

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,20 +1,25 @@
 import React from 'react'
 
 const ListItem = ({ character }) => {
-    const {name, series, events, thumbnail} = character
+    const {name, series, events, thumbnail, description} = character
+    console.log(character)
     return (      
-    <div className='character'>
-        <img src={thumbnail.path + '.' + thumbnail.extension} alt={name} />
-        <div className='character-name'>
-            {name}
-        </div>
-        <ul className='character-series'>
-            {series.items.length === 0 ? 'Sem series' : series.items.slice(0,3).map(serie => <li key={serie.key}>{serie.name.split('(')[0]}</li>)}
-        </ul>
-        <ul className='character-events'>
-            {events.items.length === 0 ? 'Sem eventos' : events.items.slice(0,3).map(event => <li key={event.key}>{event.name}</li>)}
-        </ul>         
-    </div>      
+        <div className='character'>
+            <img src={thumbnail.path + '.' + thumbnail.extension} alt={name} />
+            <div className='character-name'>
+                {name}
+            </div>
+            <ul className='character-series'>
+                {series.items.length === 0 ? 'Sem series' : series.items.slice(0,3).map((serie, key) => <li key={key}>{serie.name.split('(')[0]}</li>)}
+            </ul>
+            <ul className='character-events'>
+                {events.items.length === 0 ? 'Sem eventos' : events.items.slice(0,3).map((event, key) => <li key={key}>{event.name}</li>)}
+            </ul>         
+            <div className='char-overview'> 
+                <h4>Descrição:</h4>
+                <div>{!(description) ? 'Sem descrição': description}</div>
+            </div>
+        </div>      
     )
 }
 
